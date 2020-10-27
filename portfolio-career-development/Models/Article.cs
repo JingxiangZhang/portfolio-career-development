3d5688d6db04a0b3f04c2788f532d57e70aedeb8
Jingxaing Zhang <jzha0219@student.monash.edu>
2020-10-27 11:59:57 +0100

    create article module

---
+++ portfolio-career-development/Models/Article.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace portfolio_career_development.Models
+{
+    public class Article
+    {
+        public int articleID { get; set; }
+        public string title { get; set; }
+        public string content { get; set; }
+    }
+}
